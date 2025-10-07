1ea4b9e2119f5581e4af58cc5b314bfd3d5068cf
Ahmed Adeyemi Bello <58624027+IshCodeBellz@users.noreply.github.com>
2025-10-07 01:01:25 +0100

    Ecom:[Feature] - reviews, catergories, so much more

--- jest.env.setup.js
+++ jest.env.setup.js
@@ -1,5 +1,7 @@
 // jest.env.setup.js
+// Try to load .env.local first, then fallback to .env
 require("dotenv").config({ path: ".env.local" });
+require("dotenv").config({ path: ".env" });
 
 // Optionally log for debug:
 console.log("Jest loaded DATABASE_URL:", process.env.DATABASE_URL);
